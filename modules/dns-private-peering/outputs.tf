45792930058c36ecc20cc3a9f83c24e343e53a95
Rajdeep Mukherjee <rajdeepmj@google.com>
2022-06-15 10:44:53 +0000

    added name to outputs for dns

--- modules/dns-private-peering/outputs.tf
+++ modules/dns-private-peering/outputs.tf
@@ -2,3 +2,6 @@
 output "id" {
     value = google_dns_managed_zone.private-zone.id
 }
+output "name" {
+    value = var.name
+}
--- modules/dns-private/outputs.tf
+++ modules/dns-private/outputs.tf
@@ -2,3 +2,7 @@
 output "id" {
     value = google_dns_managed_zone.private-zone.id
 }
+
+output "name" {
+    value = var.name
+}
