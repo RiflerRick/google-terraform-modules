ec5b54c94d9b6f6d25bc3562bdac842c7dce8243
Rajdeep Mukherjee <rajdeepmj@google.com>
2022-06-21 10:28:33 +0000

    fixed dns record set

--- modules/dns-record-set/main.tf
+++ modules/dns-record-set/main.tf
@@ -8,17 +8,17 @@ resource "google_dns_record_set" "default" {
 
   rrdatas = length(var.rrdatas) > 0 ? var.rrdatas : null
   dynamic "routing_policy" {
-      for_each = length(var.routing_policies.geo_routing_policies) > 0 || length(var.routing_policies.wrr_routing_policies) > 0 ? [var.routing_policies] : []
+      for_each = length(var.routing_policies.geo_routing_policies) > 0 ? [var.routing_policies.geo_routing_policies] : length(var.routing_policies.wrr_routing_policies) > 0 ? [var.routing_policies.wrr_routing_policies] : []
       content {
           dynamic "geo" {
-              for_each = routing_policy.value.geo_routing_policies
+              for_each = routing_policy.value
               content {
                   location = geo.value.location
                   rrdatas = geo.value.rrdatas
               }
           }
           dynamic "wrr" {
-              for_each = routing_policy.value.wrr_routing_policies
+              for_each = routing_policy.value
               content {
                   weight = wrr.value.weight
                   rrdatas = wrr.value.rrdatas
