7c1e320a43fa2e8ea458d49dcaf947891cff92b4
Rajdeep Mukherjee <rajdeepmj@google.com>
2022-06-21 07:50:23 +0000

    fixed dns private

--- modules/dns-private/main.tf
+++ modules/dns-private/main.tf
@@ -5,7 +5,7 @@ resource "google_dns_managed_zone" "private-zone" {
   project = var.project_id
 
   dynamic "private_visibility_config" {
-      for_each = for_each = length(var.network_self_links) > 1 ? [var.network_self_links[0]] : var.network_self_links
+      for_each = length(var.network_self_links) > 1 ? [var.network_self_links[0]] : var.network_self_links
       content {
         dynamic "networks" {
             for_each = var.network_self_links
